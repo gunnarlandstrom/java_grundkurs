6de8fdd5dd14eaeee5da839c670eab6caee427ad
gunnarlandstrom <gunnar.landstrom@hotmail.com>
2025-01-21 19:32:51 +0100

    java

---
+++ uppgifter/JavaI.java
@@ -0,0 +1,8 @@
+package uppgifter;
+
+public class JavaI {
+
+    public static void main(String[] arguments) {
+        System.out.println("Hello");
+    }
+}
--- uppgifter/uppgift_1/JavaI.java
+++
@@ -1,7 +0,0 @@
-package uppgifter.uppgift_1;
-
-public class JavaI {
-    public static void main(String[] args) {
-        System.out.println("Datateknik GR(A), Java I");
-    }
-}
