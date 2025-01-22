c33abfac827cb37e89107d0fa7465dea04888338
Gunnar <gunnar.landstrom@hotmail.com>
2025-01-22 10:09:13 +0100

    java2241

--- uppgifter/Variables.java
+++ uppgifter/Variables.java
@@ -1,10 +1,28 @@
+package uppgifter;
 public class Variables {
     public static void main(String[] args) {
         int number;
         boolean isEven;
-        int input;
+        String output;
 
-        System.
+        number = 512 % 2;
+        output = "Number is even, that is ";
+        if (number != 0) {
+            isEven = false;
+        }
+        else {
+            isEven = true;
+        }
+
+        for (int i = 0; i < 10; i++) {
+            System.out.println();
+        }
+        if (isEven) {
+            System.out.println(output);
+            System.out.print(isEven + ".");
+            
+
+        }
 
     }
 }
