afebccbf2ab0458c0f8926a9345755b35b83639f
Gunnar <gunnar.landstrom@hotmail.com>
2025-02-02 21:29:25 +0100

    labb3_2_1

---
+++ uppgifter/labb_3/CD.java
@@ -0,0 +1,55 @@
+package labb_3;
+import labb_3.Publisher;
+
+import java.util.Scanner;
+
+public class CD {
+    private String songTitle;
+    private String songArtist;
+    private int songLength;
+    private String songPublisher;
+    private String songPublisherPhone;
+
+    // Title Set/Get
+    public void setSongTitle(String input) {
+        this.songTitle = input;
+    }
+
+    public String getSongTitle() {
+        return songTitle;
+    }
+
+    // Artist Set/Get
+    public void setSongArtist(String input) {
+        this.songArtist = input;
+    }
+
+    public String getSongArtist() {
+        return songArtist;
+    }
+
+    // Length Set/Get
+    public void setSongLength(int input) {
+        this.songLength = input;
+    }
+
+    public int getSongLength() {
+        return songLength;
+    }
+
+    // Publisher Set/Get
+    public void setSongPublisher(String input) {
+        this.songPublisher = input;
+    }
+
+    public Publisher getSongPublisher() {
+        Publisher getPublisherName();
+        return
+    }
+
+    public void setSongPublisher(String name, String phone){
+        this.songPublisher = name;
+        this.songPublisherPhone = phone;
+    }
+
+}
