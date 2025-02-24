d9ecb63eaf2bf66c38ab8c016eb6155ebfb038e0
gunnarlandstrom <gunnar.landstrom@hotmail.com>
2025-02-24 13:53:26 +0100

    labb 6 fortsettning

--- labbar/src/labb_6/CountSentence.java
+++ labbar/src/labb_6/CountSentence.java
@@ -6,60 +6,90 @@ public class CountSentence {
 
     private int amountOfSentences;
     private int amountOfWords;
-    private int amountOfSpecialChars;
+    private int amountOfChars;
+    private boolean wantsToCount = true;
     private static String inputPrompt = "input> ";
     Scanner scannerInput = new Scanner(System.in);
 
     // Constructors
     public CountSentence() {
-        boolean wantsToPlay = start();
+        wantsToCount = start();
 
-        if (wantsToPlay) {
+        if (wantsToCount) {
+            System.out.println("");
             System.out.println("Enter the string you want to count!");
             System.out.print(inputPrompt);
             String userInput = scannerInput.nextLine();
-            new CountSentence(userInput);
+            String countedSentence = countingSentence(userInput);
         } else {
             quitProgram();
         }
     }
 
-    public CountSentence(String userInput){
+    public String countingSentence(String userInput) {
 
+        sentenceCounter(userInput);
+        wordCounter(userInput);
+        charCounter(userInput);
 
+        countingPrinter();
 
-
+        return "a";
     }
-
-
 
     public boolean start() {
 
         System.out.println(
                 "This is a program that will count the amount of sentences, words and special characters you type.");
 
-        System.out.println("Do you want to try it? y/n");
-        String userInput = scannerInput.nextLine();
+        while (wantsToCount) {
 
-        if (userInput.equals("y") || userInput.equals("Y")) {
-            return true;
-        } else {
-            return false;
+            System.out.println("Do you want to try it? y/n");
+
+            System.out.print(inputPrompt);
+            String userInput = scannerInput.nextLine();
+
+            if (userInput.equals("y") || userInput.equals("Y")) {
+                return true;
+            } else if (userInput.equals("n") || userInput.equals("N")) {
+                return false;
+            } else {
+                System.out.println("");
+                System.out.println("Something went wrong, try again.");
+                System.out.print(inputPrompt);
+                continue;
+            }
         }
-
+        return false;
     }
 
     public void quitProgram() {
+        System.out.println("Closing program, goodbye!");
+
         scannerInput.close();
 
         System.exit(0);
 
     }
 
-    // Sentences
-    public void sentenceCounter() {
+    public void countingPrinter() {
 
-        amountOfSentences++;
+        System.out.println("Sentences: " + amountOfSentences);
+        System.out.println("Words: " + amountOfWords);
+        System.out.println("Special characters: " + amountOfChars);
+    }
+
+    // Sentences
+    public void sentenceCounter(String userInput) {
+
+        for (int i = 0; i < userInput.length(); i++) {
+            if (userInput.charAt(i) == '.' || userInput.charAt(i) == '?' || userInput.charAt(i) == '!'
+                    || userInput.charAt(i) == 13) {
+                amountOfSentences++;
+            }
+
+        }
+
     }
 
     public int getSentences() {
@@ -68,9 +98,15 @@ public class CountSentence {
     }
 
     // Words
-    public void wordCounter() {
+    public void wordCounter(String userInput) {
 
+        for (int i = 0; i < userInput.length(); i++) {
+            if (userInput.charAt(i) == ' ') {
+                amountOfWords++;
+            }
+        }
         amountOfWords++;
+
     }
 
     public int getWords() {
@@ -79,14 +115,16 @@ public class CountSentence {
     }
 
     // Characters
-    public void charCounter() {
+    public void charCounter(String userInput) {
 
-        amountOfSpecialChars++;
+        for (int i = 0; i < userInput.length(); i++) {
+            amountOfChars++;
+        }
     }
 
     public int getCharacters() {
 
-        return amountOfSpecialChars;
+        return amountOfChars;
     }
 
 }
